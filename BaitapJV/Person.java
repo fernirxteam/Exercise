dd3bab7d417454996d49f13de46fda0429036ecd
hungmonkeydev <hunggithubteam@gmail.com>
2024-03-13 09:30:13 +0700

    Tao thu muc: class Person va QLCB

---
+++ BaitapJV/Person.java
@@ -0,0 +1,51 @@
+package Baitap.Exercise;
+
+public class Person {
+    private String name;
+    private int age;
+    private String homeTown;
+    private String teacherCode;
+
+    public Person(){
+        
+    }
+
+    public Person(String name, int age, String homeTown, String teacherCode) {
+        this.name = name;
+        this.age = age;
+        this.homeTown = homeTown;
+        this.teacherCode = teacherCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHomeTown() {
+        return homeTown;
+    }
+
+    public void setHomeTown(String homeTown) {
+        this.homeTown = homeTown;
+    }
+
+    public String getTeacherCode() {
+        return teacherCode;
+    }
+
+    public void setTeacherCode(String teacherCode) {
+        this.teacherCode = teacherCode;
+    }
+}
